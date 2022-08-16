a179a284ef961528456643417594fec85c688c26
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-08-16 13:03:46 +0300

    edited modal html

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,16 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+
+  (() => {
+    document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+      e.preventDefault();
+  
+      new FormData(e.currentTarget).forEach((value, name) =>
+        console.log(`${name}: ${value}`),
+      );
+
+      e.currentTarget. reset();
+    });
+  })();
